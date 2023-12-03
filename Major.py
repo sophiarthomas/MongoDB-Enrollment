be4a351890d389c24f774e7049abb5b36f3db2ff
Sophia Thomas <107733211+sophiarthomas@users.noreply.github.com>
2023-12-02 18:59:44 -0800

    Major Finished

--- Major.py
+++ Major.py
@@ -1,5 +1,6 @@
 import pymongo
 from pprint import pprint
+import Department
 
 def create_major_schema(db):
 	major_validator={
@@ -7,13 +8,18 @@ def create_major_schema(db):
 			'$jsonSchema': {
 				'bsonType': "object",
 				'description': 'A specific area of study in which a student chooses to specialize',
-				'required': ['name'],
+				'required': ['department_abbreviation', 'name'],
 				'additionalProperties': False,
 				'properties': {
 					'_id': {},
+					'department_abbreviation': {
+						'bsonType': 'string',
+						'description': 'embedded department abbreviation the course falls under',
+						'maxLength': 6
+					},
 					'name': {
 						'bsonType': 'string',
-						'description': 'given name of the major'
+						'description': 'a text string that identifies the major'
 					}
 				}
 			}
@@ -32,14 +38,21 @@ def create_major_schema(db):
 
 def add_major(db):
 	collection = db["majors"]
+	department = Department.select_department(db)
 	while True:
 		try:
 			name = input("Major name--> ")
 
 			major = {
+				"department_abbreviation": department.get("abbreviation"),
 				"name": name
 			}
 			collection.insert_one(major)
+			db.departments.update_one(
+				{"_id": department.get("_id")},
+				{"$push": {"majors": {"major_id": major.get("_id"),
+									  "major_name": name}}}
+			)
 			print("Major added successfully.")
 			break
 		except Exception as e:
@@ -62,10 +75,28 @@ def select_major(db):
 
 
 def delete_major(db):
+	collection = db["majors"]
 	major = select_major(db)
-	majors = db["majors"]
-	deleted = majors.delete_one({"_id": major["_id"]})
-	print(f"We just deleted: {deleted.deleted_count} majors")
+	#ensure there are no student_majors with this major before deleting
+	#...
+	search = {
+		"student_major":  {
+			"$elemMatch": {
+				"major_name": major.get("name")
+			}
+		}
+	}
+	declared_count = db.students.count_documents(search)
+	if declared_count == 0:
+		db.departments.update_one(
+			{"abbreviation": major.get("department_abbreviation")},
+			{"$pull": {"majors": {"major_name": major.get("name")}}}
+		)
+		deleted = collection.delete_one({"_id": major["_id"]})
+		print(f"We just deleted: {deleted.deleted_count} majors")
+	else:
+		print(f"There are {declared_count} students declared to that major.\n"
+			  f"Undeclare them first, then come back here to delete the major.")
 
 
 def list_major(db):
